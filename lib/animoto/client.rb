69b6b913fd33df4085ced383d6e7f8d1bca98ab9
Sander Hartlage <sander@animoto.com>
2010-08-04 14:08:39 -0400

    fixed requires

--- lib/animoto/client.rb
+++ lib/animoto/client.rb
@@ -1,9 +1,6 @@
 here = File.dirname(__FILE__)
 require here + '/asset'
-require here + '/http_engine'
-require here + '/http_response'
-require here + '/manifest'
-require here + '/response_parser'
+require here + '/project'
 require here + '/resource'
 require here + '/storyboard'
 require here + '/video'
@@ -11,7 +8,6 @@ require here + '/job'
 require here + '/directing_and_rendering_job'
 require here + '/directing_job'
 require here + '/rendering_job'
-
 
 module Animoto
   class Client
