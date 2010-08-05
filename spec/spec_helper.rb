15dd1b16e7c0b8741f3dcc91d79eae4af4839d39
Sander Hartlage <sander@animoto.com>
2010-08-05 18:24:30 -0400

    added webmock to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'spec'
+require 'webmock/rspec'
 require 'mocha'
 
 Spec::Runner.configure do |config|
   config.mock_with :mocha
+  config.include WebMock
 end
 
 require File.dirname(__FILE__) + '/../lib/animoto/client'
