d80c01a15dbd09e8595449eeb5211033afcc3781
Sander Hartlage <sander@animoto.com>
2010-08-05 18:26:09 -0400

    changed class-level getter/setter methods to single combo-getter-setter type

--- lib/animoto/resource.rb
+++ lib/animoto/resource.rb
@@ -1,36 +1,31 @@
 module Animoto
   class Resource
     
-    def self.endpoint
+    def self.endpoint path = nil
+      @endpoint = path if path
       @endpoint
-    end
-    
-    def self.endpoint= path
-      @endpoint = path
     end
     
     def endpoint
       self.class.endpoint
     end
     
-    def self.content_type
+    def self.content_type type = nil
+      @content_type = type if type
       @content_type
-    end
-    
-    def self.content_type= type
-      @content_type = type
     end
     
     def content_type
       self.class.content_type
     end
     
+    def self.load body, headers
+      
+    end
+    
     def initialize *args
       
     end
 
-    def to_request_body
-      
-    end
   end
 end
