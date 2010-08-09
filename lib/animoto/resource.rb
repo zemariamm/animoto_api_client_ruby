19b2be4294b4f55504c04b8b669be882e71f8adc
Sander Hartlage <sander@animoto.com>
2010-08-09 17:52:10 -0400

    added new ContentType module into Resource

--- lib/animoto/resource.rb
+++ lib/animoto/resource.rb
@@ -1,5 +1,6 @@
 module Animoto
   class Resource
+    include ContentType
     
     def self.endpoint path = nil
       @endpoint = path if path
@@ -8,20 +9,6 @@ module Animoto
     
     def endpoint
       self.class.endpoint
-    end
-    
-    def self.content_type type = nil
-      @content_type = type if type
-      @content_type ||= infer_content_type
-    end
-    
-    def self.infer_content_type
-      name.split('::').last.gsub(/(^)?([A-Z])/) { "#{'_' unless $1}#{$2.downcase}" }
-    end
-    private_class_method :infer_content_type
-    
-    def content_type
-      self.class.content_type
     end
     
     def self.payload_key key = nil
