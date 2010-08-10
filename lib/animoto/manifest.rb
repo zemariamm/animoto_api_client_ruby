04d8034bd2582ac8e70554c32757d75bf72ff69d
Sander Hartlage <sander@animoto.com>
2010-08-10 18:08:23 -0400

    tiny little change

--- lib/animoto/manifest.rb
+++ lib/animoto/manifest.rb
@@ -4,12 +4,14 @@ module Animoto
     
     # Returns a representation of this manifest as a Hash, used to populate
     # request bodies when directing, rendering, etc.
+    #
     # @return [Hash] the manifest as a Hash
     def to_hash
       {}
     end
     
     # Returns a representation of this manifest as JSON.
+    #
     # @return [String] the manifest as JSON
     def to_json
       self.to_hash.to_json
