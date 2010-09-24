cde54debc928fb9fd5ed963f99fd74243a56b7b3
Sander Hartlage <sander@animoto.com>
2010-09-24 11:59:06 -0400

    put payload_key stuff into standard envelope module, which now automatically includes the content type module

--- lib/animoto/support/standard_envelope.rb
+++ lib/animoto/support/standard_envelope.rb
@@ -6,6 +6,7 @@ module Animoto
         base.class_eval {
           include Animoto::Support::StandardEnvelope::InstanceMethods
           extend Animoto::Support::StandardEnvelope::ClassMethods
+          include Animoto::Support::ContentType
         }
       end
     
@@ -13,14 +14,39 @@ module Animoto
         def unpack_standard_envelope body = {}
           self.class.unpack_standard_envelope body
         end
+
+        # Returns the payload key for this class.
+        #
+        # @return [String] the key
+        def payload_key
+          self.class.payload_key
+        end
       end
     
       module ClassMethods
-        protected
+        # @overload payload_key(key)
+        #   Sets the payload key for this class. When building an instance of this class from
+        #   a response body, the payload key determines which object in the response payload
+        #   holds the attributes for the instance.
+        #
+        #   @param [String] key the key to set
+        #   @return [String] the key
+        #   
+        # @overload payload_key()
+        #   Returns the payload key for this class.
+        #
+        #   @return [String] the key
+        def payload_key key = nil
+          @payload_key = key if key
+          @payload_key || infer_content_type
+        end
+
+        protected        
+
         def unpack_standard_envelope body = {}
           {
             :url => body['response']['payload'][payload_key]['links']['self'],
-            :errors => body['response']['status']['errors'] || []
+            :errors => body['response']['status'] ? (body['response']['status']['errors'] || []) : []
           }
         end
       end
