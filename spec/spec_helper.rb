dbb387612e958890bb9d127e7c6f6e02c8e6570e
Sander Hartlage <sander@animoto.com>
2010-10-21 11:31:10 -0400

    updated WebMock usage to remove deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'mocha'
 
 Spec::Runner.configure do |config|
   config.mock_with :mocha
-  config.include WebMock
+  config.include WebMock::API
 end
 
 require File.expand_path(File.dirname(__FILE__) + '/../lib/animoto/client')
