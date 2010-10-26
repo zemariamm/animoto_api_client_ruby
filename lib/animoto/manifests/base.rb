ac33c9235b74ff1728238f3da26b28bf850b8218
Sander Hartlage <sander@animoto.com>
2010-10-26 11:48:10 -0400

    fixed inferring content type in manifests

--- lib/animoto/manifests/base.rb
+++ lib/animoto/manifests/base.rb
@@ -5,6 +5,11 @@ module Animoto
     class Base
       include Support::ContentType
     
+      # @return [String]
+      def self.infer_content_type
+        super + '_manifest'
+      end
+            
       # Returns a representation of this manifest as a Hash, used to populate
       # request bodies when directing, rendering, etc.
       #
