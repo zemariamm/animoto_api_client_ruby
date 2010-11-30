6fb9dca388b51cc9a82f662eeea5df8c28d80ca7
Sander Hartlage <sander@animoto.com>
2010-11-30 17:54:00 -0500

    added a bunch of stuff to simplify standard envelope unpacking

--- lib/animoto/support/standard_envelope.rb
+++ lib/animoto/support/standard_envelope.rb
@@ -11,12 +11,25 @@ module Animoto
           include Animoto::Support::ContentType
         }
       end
+      
+      def self.find_class_for envelope
+        if payload_key = ((envelope['response'] || {})['payload'] || {}).keys.first
+          klass_name = payload_key.camelize
+          if /(?:Job|Callback)$/ === klass_name
+            Animoto::Jobs::const_get(klass_name) if Animoto::Jobs::const_defined?(klass_name)
+          else
+            Animoto::Resources::const_get(klass_name) if Animoto::Resources::const_defined?(klass_name)
+          end
+        end
+      rescue NameError
+        nil
+      end
     
       module InstanceMethods
         # Calls the class-level unpack_standard_envelope method.
         # @return [Hash<Symbol,Object>]
         # @see Animoto::Support::StandardEnvelope::ClassMethods#unpack_standard_envelope
-        def unpack_standard_envelope body = {}
+        def unpack_standard_envelope body
           self.class.unpack_standard_envelope body
         end
 
@@ -49,17 +62,50 @@ module Animoto
 
         protected        
 
+        # Extracts the base payload element from the envelope.
+        #
+        # @param [Hash<String,String>] body the response body
+        # @return [Hash<String,String>] the base payload at $.response.payload
+        def unpack_base_payload body
+          (body['response'] || {})['payload'] || {}
+        end
+        
+        # Extracts the base status element from the envelope.
+        #
+        # @param [Hash<String,String>] body the response body
+        # @return [Hash<String,String>] the status element at $.response.status
+        def unpack_status body
+          (body['response'] || {})['status'] || {}
+        end
+
+        # Returns the payload, which is the part of the response classes will find the most
+        # interesting.
+        #
+        # @param [Hash<String,String>] body the response body
+        # @return [Hash<String,String>] the payload at $.response.payload[${payload_key}]
+        def unpack_payload body
+          unpack_base_payload(body)[payload_key] || {}
+        end
+        
+        # Returns the links element of the payload, or an empty hash if it doesn't exist.
+        # 
+        # @param [Hash<String,String>] body the response body
+        # @return [Hash<String,String>] the links at $.response.payload[${payload_key}].links
+        def unpack_links body
+          unpack_payload(body)['links'] || {}
+        end
+
         # Extracts common elements from the 'standard envelope' and returns them in a
         # easier-to-work-with hash.
         #
         # @param [Hash<String,Object>] body the body, structured in the 'standard envelope'
         # @return [Hash<Symbol,Object>] the nicer hash
-        def unpack_standard_envelope body = {}
+        def unpack_standard_envelope body
           {
-            :url => body['response']['payload'][payload_key]['links']['self'],
-            :errors => body['response']['status'] ? (body['response']['status']['errors'] || []) : []
+            :url    => unpack_links(body)['self'],
+            :errors => unpack_status(body)['errors'] || []
           }
-        end
+        end        
       end
     end
   end
