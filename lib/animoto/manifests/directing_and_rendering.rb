9b415a3940993925bbc9e3fb13fa7ca42fdd95e9
Sander Hartlage <sander@animoto.com>
2010-11-30 18:15:20 -0500

    simplified directing and rendering manifests
    instead of being a subclass of directing manifests and reimplmenting everything from rendering manifests, now a directing and rendering manifest just simply builds two sub-manifests and then delegates methods to them, then embeds their hash representations into its own

--- lib/animoto/manifests/directing_and_rendering.rb
+++ lib/animoto/manifests/directing_and_rendering.rb
@@ -1,48 +1,75 @@
 module Animoto
   module Manifests
-    class DirectingAndRendering < Animoto::Manifests::Directing
+    
+    # A directing-and-rendering manifest is little more than just a single envelope
+    # with a directing manifest and a rendering manifest embedded within.
+    class DirectingAndRendering < Animoto::Manifests::Base
       
-      # The vertical resolution of the rendered video. Valid values are '180p', '270p',
-      # '360p', '480p', '576p', '720p' or '1080p'.
+      # The embedded directing manifest
+      # @return [Manifests::Directing]
+      attr_reader :directing_manifest
+      
+      # The embedded rendering manifest
+      # @return [Manifests::Rendering]
+      attr_reader :rendering_manifest
+
+      # A URL to receive a callback after directing is finished.
       # @return [String]
-      attr_accessor :resolution
+      attr_accessor :http_callback_url
       
-      # The framerate of the rendered video. Valid values are 12, 15, 24 or 30.
-      # @return [Fixnum]
-      attr_accessor :framerate
-      
-      # The format of the rendered video. Valid values are 'h264', 'h264-iphone', 'flv' or 'iso'.
+      # The format of the callback; either 'xml' or 'json'.
       # @return [String]
-      attr_accessor :format
-      
+      attr_accessor :http_callback_format
+
       # Creates a new directing-and-rendering manifest.
       #
       # @param [Hash<Symbol,Object>] options
+      # @option options [String] :title the title of this project
+      # @option options [String] :pacing ('default') the pacing for this project
       # @option options [String] :resolution the vertical resolution of the rendered video
       # @option options [Integer] :framerate the framerate of the rendered video
       # @option options [String] :format the format of the rendered video
-      # @return [Manifests::DirectingAndRendering] the manifest
-      # @see Animoto::Manifests::Directing#initialize
-      def initialize options = {}
-        super
-        @resolution = options[:resolution]
-        @framerate  = options[:framerate]
-        @format     = options[:format]
+      # @option options [String] :http_callback_url a URL to receive a callback when this job is done
+      # @option options [String] :http_callback_format the format of the callback
+      def initialize options = {}        
+        @directing_manifest = Manifests::Directing.new(options.only(:title, :pacing))
+        @rendering_manifest = Manifests::Rendering.new(options.only(:resolution, :framerate, :format))
+        
+        @http_callback_url  = options[:http_callback_url]
+        @http_callback_format = options[:http_callback_format]
       end
-    
+
+      # Delegates method calls to the underlying directing or rendering manifests if
+      # they respond to the call.
+      #
+      # @raise [NoMethodError] if the underlying manifests don't respond
+      def method_missing *args
+        name = args.first
+        if directing_manifest.respond_to?(name)
+          directing_manifest.__send__ *args
+        elsif rendering_manifest.respond_to?(name)
+          rendering_manifest.__send__ *args
+        else
+          super
+        end
+      end
+
       # Returns a representation of this manifest as a Hash.
       #
       # @return [Hash<String,String>] the manifest as a Hash
       # @raise [ArgumentError] if a callback URL is specified but not the format
       # @see Animoto::Manifests::Directing#to_hash
+      # @see Animoto::Manifests::Rendering#to_hash
       def to_hash options = {}
-        hash  = super
-        directing_job = hash.delete('directing_job')
-        hash['directing_and_rendering_job'] = directing_job.merge('rendering_manifest' => { 'rendering_parameters' => {}})
-        params = hash['directing_and_rendering_job']['rendering_manifest']['rendering_parameters']
-        params['resolution'] = resolution
-        params['framerate'] = framerate
-        params['format'] = format
+        hash  = { 'directing_and_rendering_job' => {} }
+        job   = hash['directing_and_rendering_job']
+        if http_callback_url
+          raise ArgumentError, "You must specify a http_callback_format (either 'xml' or 'json')" if http_callback_format.nil?
+          job['http_callback'] = http_callback_url
+          job['http_callback_format'] = http_callback_format
+        end
+        job['directing_manifest'] = self.directing_manifest.to_hash['directing_job']['directing_manifest']
+        job['rendering_manifest'] = self.rendering_manifest.to_hash['rendering_job']['rendering_manifest']
         hash
       end
     
